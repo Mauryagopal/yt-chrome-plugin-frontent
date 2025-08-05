adbb63c584200eebefa03a649ff901f25a101b00
vermagaurav24 <gaurav123cse@gmail.com>
2025-08-06 00:45:27 +0530

    api key added

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", async () => {
   const outputDiv = document.getElementById("output");
-  const API_KEY = 'YOUR_API_KEY';  // Replace with your actual YouTube Data API key
+  const API_KEY = 'AIzaSyAJilUCsG0bzVBzmgJQd5Ub8BHpY9wZsug';  // Replace with your actual YouTube Data API key
   const API_URL = 'http://my-elb-2062136355.us-east-1.elb.amazonaws.com:80';
 
   // Get the current tab's URL
@@ -240,4 +240,4 @@ document.addEventListener("DOMContentLoaded", async () => {
       outputDiv.innerHTML += "<p>Error fetching trend graph image.</p>";
     }
   }
-});
+}); 
